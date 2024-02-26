8bcd189dce585f45a3390eadb6b4e9de402e1003
dvargasl2 <d.vargasl2@uniandes.edu.co>
2024-02-26 00:22:04 -0500

    Comienzo de implementacion

--- src/uniandes/dpoo/aerolinea/persistencia/IPersistenciaAerolinea.java
+++ src/uniandes/dpoo/aerolinea/persistencia/IPersistenciaAerolinea.java
@@ -1,5 +1,8 @@
 package uniandes.dpoo.aerolinea.persistencia;
+import uniandes.dpoo.aerolinea.modelo.Aerolinea;
 
-public class IPersistenciaAerolinea {
 
+public interface IPersistenciaAerolinea {
+	public void cargarAereolinea(String archivo, Aerolinea aereolinea);
+	public void salvarAerolinea(String archivo, Aerolinea aereolinea);
 }
---
+++ src/uniandes/dpoo/aerolinea/persistencia/PersistenciaAereolineaJson.java
@@ -0,0 +1,13 @@
+package uniandes.dpoo.aerolinea.persistencia;
+
+import uniandes.dpoo.aerolinea.modelo.Aerolinea;
+
+public class PersistenciaAereolineaJson {
+	public void cargarAereolinea(String archivo, Aerolinea aereolinea) {
+		
+	}
+	public void salvarAerolinea(String archivo, Aerolinea aereolinea) {
+		
+	}
+}
+
